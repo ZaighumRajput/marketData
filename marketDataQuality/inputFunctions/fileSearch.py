69c99856d74ecd48fe8a748d8e40dc53eb43b411
Zaighum Rajput <zaighum47@gmail.com>
2014-09-12 16:46:42 +0200

    filenames, read into pandas

--- marketDataQuality/inputFunctions/fileSearch.py
+++ marketDataQuality/inputFunctions/fileSearch.py
@@ -19,8 +19,8 @@ def findFile(marketDataFolder, date, what, source="IMDB"):
                 matches.append(f)
      return matches[0]
 
-def findFile2(marketDataFolder, date, what, source="IMDB"):
-    print "I am searching in", marketDataFolder, "for", what, "at date", date 
+def findFile2(date, what, marketDataFolder,source="IMDB", DEBUG=False):
+    if(DEBUG): print "I am searching in", marketDataFolder, "for", what, "at date", date 
     #the pattern will depend on source; need to change
     pattern = filePattern(date, what)
     fileName = re.compile(pattern + "*")
@@ -31,57 +31,56 @@ def findFile2(marketDataFolder, date, what, source="IMDB"):
         #print directory
         if dirName.match(directory):
             
-            print "I found the directory", directory
+            if(DEBUG): print "I found the directory", directory
             marketDataDirectory = os.path.join(marketDataFolder, directory)
              
             for files in os.listdir(marketDataDirectory):
                 #print files
 
                 if fileName.match(files):
-                    print "The file that matches is", files
+                    if(DEBUG):
+                        print "The file that matches is", files
+                        print "The full path is", os.path.join(marketDataDirectory, files)
                     return os.path.join(marketDataDirectory, files)
     
-           
-   
-
-
-    
-
-
 def filePattern(date, what, type = "IMDB"):
     if type == "IMDB":
         return "IMDB_" + date + "_MD" + what + "_R_"
+    
+if __name__ == "__main__":
 
 
-#change date
-def test_correctFile(marketDataFolder,date, what ):
-    '''See if it find the correct file
-    '''
-    #print marketDataFolder
-    print findFile2(marketDataFolder,date, what)  
-    #print findFile(marketDataFolder,date, what)
 
 
-def test_newestFile(marketDataFolder, date, what):
-    '''If they are duplicate finds 
-    it should return the newest one
-    '''
+    #change date
+    def test_correctFile(marketDataFolder,date, what ):
+        '''See if it find the correct file
+        '''
+        #print marketDataFolder
+        print findFile2(marketDataFolder,date, what)  
+        #print findFile(marketDataFolder,date, what)
 
-def test_filePattern():
-    assert filePattern("20140818", "IDXIV", "IMDB") == "IMDB_20140818_MDIDXIV_R_"
+
+    def test_newestFile(marketDataFolder, date, what):
+        '''If they are duplicate finds 
+        it should return the newest one
+        '''
+
+    def test_filePattern():
+        assert filePattern("20140818", "IDXIV", "IMDB") == "IMDB_20140818_MDIDXIV_R_"
 
 
-currentFolder = os.path.dirname(__file__)
-MD = os.path.join(currentFolder, "fileSearchTest", "MD")
-networkMD = r"//srpzyfap0003.insim.biz/ESGShare/MD"
+    currentFolder = os.path.dirname(__file__)
+    MD = os.path.join(currentFolder, "fileSearchTest", "MD")
+    networkMD = r"//srpzyfap0003.insim.biz/ESGShare/MD"
 
     
     
-#use nose to run later
-test_correctFile(networkMD, "20140818", "FX") 
-#test_correctFile(MD, "20140814", "FX")
+    #use nose to run later
+    test_correctFile("20140818", "FX", networkMD) 
+    #test_correctFile(MD, "20140814", "FX")
 
-test_filePattern()
+    test_filePattern()
 
 
 
---
+++ marketDataQuality/inputFunctions/pandaLoad.py
@@ -0,0 +1,42 @@
+"""This package takes 
+"""
+import pandas as pd
+import fileSearch
+
+def getDataSeries(date, what,marketDataFolder, dataSpecific=[], source="IMDB", DEBUG=False):
+    '''Returns a pandas dataFrame
+    Data series specific files come in a list
+    
+
+
+    ---
+    getDataSeries("20140818", FX) => pd.df
+    getDataSeries("20140818", FX, ['JPY']) => subset of pdf.df
+    '''
+    filePath= fileSearch.findFile2(date, what, marketDataFolder)
+    if(DEBUG): "I am going to try to read in the following file", filePath
+        
+    dataSeries = pd.read_csv(filePath, usecols=[2,14,17], skipfooter=1, skiprows=1, header=None)
+    arguments = "pd.read_csv(filePath)"
+    print dataSeries
+    dataSeries1 = eval(arguments)
+
+
+
+#Which columns are important
+     
+
+
+
+if __name__ == "__main__":
+
+    def test_getDataSeries():
+        networkMD = r"//srpzyfap0003.insim.biz/ESGShare/MD"
+        getDataSeries("20140818", "IDXIV", networkMD, DEBUG = True)
+
+    
+    test_getDataSeries()
+
+    
+    
+
