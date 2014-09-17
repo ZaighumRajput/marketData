b23f53386cfc7bcd424d6441dda9070e6d0c39a8
Zaighum Rajput <zaighum47@gmail.com>
2014-09-17 16:17:18 +0200

    Reading in works

--- marketDataQuality/inputFunctions/pandaLoad.py
+++ marketDataQuality/inputFunctions/pandaLoad.py
@@ -6,8 +6,6 @@ import fileSearch
 def getDataSeries(date, what,marketDataFolder, dataSpecific=[], source="IMDB", DEBUG=False):
     '''Returns a pandas dataFrame
     Data series specific files come in a list
-    
-
 
     ---
     getDataSeries("20140818", FX) => pd.df
@@ -15,17 +13,20 @@ def getDataSeries(date, what,marketDataFolder, dataSpecific=[], source="IMDB", D
     '''
     filePath= fileSearch.findFile2(date, what, marketDataFolder)
     if(DEBUG): "I am going to try to read in the following file", filePath
-        
-    dataSeries = pd.read_csv(filePath, usecols=[2,14,17], skipfooter=1, skiprows=1, header=None)
-    arguments = "pd.read_csv(filePath," + imdbConfig["EQIV"] + ",skipfooter=1, skiprows=1, header=None)"
-    print dataSeries
-    dataSeries1 = eval(arguments)
 
+    #TODO: Make Multindex
+    #dataSeries = pd.read_csv(filePath, usecols=[1,6,8,14,17], names=["Date", "Index", "Expiry", "Moneyness", "Volatility"], skipfooter=1, skiprows=1, index_col = [0])
+    #print dataSeries
+    arguments = "pd.read_csv(filePath," + imdbConfig[what] + ",skipfooter=1, skiprows=1, header=None)"
+    dataSeries = eval(arguments)
+    return dataSeries
 
 
 #Which columns are important
-imdbConfig = {"EQIV": "usecols=[2,14,17]"}     
-
+imdbConfig = {"IDXIV": 'usecols=[1,6,8,14,17], names=["Date", "Index", "Expiry", "Moneyness", "Volatility"], index_col = ["Date"]',
+              "FX": 'usecols = [1,5,9], names = ["Date", "Key", "Exchange Rate"], index_col =["Date"]',
+              "IRC" : 'usecols = [1,5,6,8,17], names = ["Date", "Key", "Description", "Maturity", "InterestRate"], index_col = ["Date"]' }     
+              
 
 
 if __name__ == "__main__":
@@ -36,6 +37,9 @@ if __name__ == "__main__":
 
     
     test_getDataSeries()
+
+
+
 
 
     
